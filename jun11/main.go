3f5e85c87e4dc781695b572a4a5e19b4a4ba789a
PukeGuo <guopuke@gmail.com>
2018-06-12 21:05:03 +0800

    Jun 10,11

---
+++ jun11/main.go
@@ -0,0 +1,39 @@
+package main
+
+// Unit 1
+
+// var a = "G"
+
+// func main() {
+//   n()
+//   m()
+//   n()
+// }
+
+// func n() {
+//   print(a)
+// }
+
+// func m() {
+//   a:="O"
+//   print(a)
+// }
+
+// Unit 2
+var a string
+
+func main() {
+	a = "G"
+	print(a)
+	f1()
+}
+
+func f1() {
+	a := "O"
+	print(a)
+	f2()
+}
+
+func f2() {
+	print(a)
+}
